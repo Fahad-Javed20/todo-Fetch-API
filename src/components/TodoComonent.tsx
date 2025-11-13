130ce085d22185744b87545604b81bb37500f20b
Fahad Bin Javed <126807388+Fahad-Javed20@users.noreply.github.com>
2025-11-13 23:32:21 +0500

    make changes

--- src/components/TodoComonent.tsx
+++ src/components/TodoComonent.tsx
@@ -1,14 +1,19 @@
-
-
 const TodoComonent = () => {
   return (
-    <div className="h-screen flex justify-center bg-blue-600">
-
-    <div className="h-32 w-2/3 bg-white shadow-lg/30">
-
+    <div className="w-2/3 h-20 mx-auto mt-10 p-4 bg-white rounded-xl shadow">
+      <div className="flex items-center justify-between bg-gray-50 px-3 py-2 rounded-lg">
+        <span className="font-medium">Task 1</span>
+        <input
+          type="checkbox"
+          className="w-5 h-5 text-blue-600 rounded focus:ring-blue-400"
+        />
+      </div>
     </div>
-    </div>
-  )
+  );
+};
+
+export default TodoComonent;
+
+{
+  /* <h1 className="text-xl font-semibold text-center mb-4">Todo Tasks</h1> */
 }
-
-export default TodoComonent
