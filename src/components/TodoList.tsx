f81a99ea177f7a6f02f53ffa4f6a8da7f44dbcc6
Fahad Bin Javed <126807388+Fahad-Javed20@users.noreply.github.com>
2025-11-13 23:47:04 +0500

    make changes

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,7 +1,36 @@
+import { useState } from "react"
 import TodoComonent from "./TodoComonent"
+import type { TodoType } from "../types/TodoType"
+
 
 
 const TodoList = () => {
+
+
+    const [loading,setLoading] = useState<boolean>(false)
+    const [todos,setTodos] = useState<TodoType[]>([])
+    const [errors,setErrors] = useState<string>("")
+
+    const fetchTodos = async ()=>{
+    try{
+        setLoading(true);
+        const response = await fetch('https://dummyjson.com/todos')
+            if(!response.ok){
+                throw new Error("Response is Not Okay")
+    }
+        const data = await response.json()
+        setTodos(data.todos)
+}
+catch(error){
+    console.log("There is Error in Fetcging:",error)
+    setErrors((error as Error).message)
+}
+finally{
+    setLoading(false)
+}
+
+
+
   return (
     <div>
         <TodoComonent/>
