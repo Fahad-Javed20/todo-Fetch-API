fe0d8a38bbe7ba32e6568a983e5df9241bbabe08
Fahad Bin Javed <126807388+Fahad-Javed20@users.noreply.github.com>
2025-11-13 23:17:00 +0500

    make chnages

---
+++ src/components/TodoComonent.tsx
@@ -0,0 +1,14 @@
+
+
+const TodoComonent = () => {
+  return (
+    <div className="h-screen flex justify-center bg-blue-600">
+
+    <div className="h-32 w-2/3 bg-white shadow-lg/30">
+
+    </div>
+    </div>
+  )
+}
+
+export default TodoComonent
---
+++ src/components/TodoList.tsx
@@ -0,0 +1,12 @@
+import TodoComonent from "./TodoComonent"
+
+
+const TodoList = () => {
+  return (
+    <div>
+        <TodoComonent/>
+    </div>
+  )
+}
+
+export default TodoList
